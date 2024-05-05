511710ce87b551d27161f69a96629a21c6ad78d7
Natthawut Haematulin <natthawut1991@gmail.com>
2024-05-05 23:56:36 +0700

    feat: update layouts

--- the-complete-ruby-on-rails-developer-course/README.md
+++ the-complete-ruby-on-rails-developer-course/README.md
@@ -1,3 +1,5 @@
 # The Complete Ruby on Rails Developer Course
 
 https://www.udemy.com/course/the-complete-ruby-on-rails-developer-course/
+
+Source code https://github.com/udemyrailscourse/alpha-blog-6
--- the-complete-ruby-on-rails-developer-course/alpha-blog/config/routes.rb
+++ the-complete-ruby-on-rails-developer-course/alpha-blog/config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  root "pages#home"
+  get "about", to: "pages#about"
   resources :articles
 end
