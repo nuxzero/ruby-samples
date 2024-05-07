c37bf07f6790057c9c8b3f96f590c73e400fcfb4
Natthawut Haematulin <natthawut1991@gmail.com>
2024-05-07 19:50:17 +0700

    feat: implement model association

---
+++ the-complete-ruby-on-rails-developer-course/alpha-blog/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_many :articles
+
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
+end
