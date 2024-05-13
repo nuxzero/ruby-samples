f7ec1c9c6f73ed6b3c6fe2286be6286d2105d7c8
Natthawut Haematulin <natthawut1991@gmail.com>
2024-05-13 19:21:57 +0700

    feat: add user profile page

--- the-complete-ruby-on-rails-developer-course/alpha-blog/app/helpers/users_helper.rb
+++ the-complete-ruby-on-rails-developer-course/alpha-blog/app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  # This method will return the Gravatar for the given user.
+  # it will be used in the users/show.html.erb file.
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: 'rounded mx-auto d-block')
+  end
 end
